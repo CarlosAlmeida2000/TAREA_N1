eb1c66a2766ba55644124d0fcaf9555b9c8e8160
Carlosalmeida10 <carlos.almeida2017@uteq.edu.ec>
2020-11-20 19:44:30 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tarea1/Actividad2.java
@@ -0,0 +1,39 @@
+package com.example.tarea1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.icu.util.Calendar;
+import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.text.method.DateTimeKeyListener;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class Actividad2 extends AppCompatActivity {
+    TextView lblMensajeRecibido;
+    Bundle b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad2);
+
+        lblMensajeRecibido = (TextView)findViewById(R.id.lblMensaje);
+        b = this.getIntent().getExtras();
+
+        String fechaNac = b.getString("FECHA_NAC");
+        String[] parts = fechaNac.split("/");
+        int dia = Integer.parseInt(parts[0]);
+        int mes = Integer.parseInt(parts[1]);
+        int año = Integer.parseInt(parts[2]);
+        Period er = Period.between(LocalDate.of(año, mes, dia), LocalDate.now());
+
+        lblMensajeRecibido.setText("Hola, "+(b.getString("SEXO").equals("Masculino")?"bienvenido ":"bienvenida "+" \n")+
+        b.getString("NOMBRE")+ " su fecha de nacimiento es "+ b.getString("FECHA_NAC") +". En la actualidad usted tiene " + er.getYears() + " años, su número de teléfono: "+
+        b.getString("TELEFONO") + " lo registraremos para poder contactarnos con usted \n Gracias.");
+
+    }
+}
---
+++ app/src/main/java/com/example/tarea1/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.tarea1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.app.Notification;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.icu.util.Calendar;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import java.time.LocalDate;
+
+public class MainActivity extends AppCompatActivity {
+    Intent intent;
+    Bundle b;
+    private  int dia,mes,ano;
+    EditText fecha, nombre, telefono;
+    RadioButton radGenero;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        radGenero = (RadioButton)findViewById(R.id.radMasc);
+        radGenero.setChecked(true);
+
+        fecha =(EditText)findViewById(R.id.dtFechaNac);
+        fecha.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(fecha.hasFocus()){
+                    VerCalendario(v);
+                }
+            }
+        });
+    }
+    public void VerCalendario(View view){
+        final Calendar c= Calendar.getInstance();
+        dia=c.get(Calendar.DAY_OF_MONTH);
+        mes=c.get(Calendar.MONTH);
+        ano=c.get(Calendar.YEAR);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                fecha.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+            }
+        }
+                ,dia,mes,ano);
+        datePickerDialog.show();
+    }
+    public void EnviarDatos(View view){
+        intent = new Intent(MainActivity.this, Actividad2.class);
+
+        nombre = (EditText) findViewById(R.id.txtNombre);
+        radGenero = (RadioButton)findViewById(R.id.radMasc);
+        if(!radGenero.isChecked()){
+            radGenero = (RadioButton)findViewById(R.id.radFemenino);
+        }
+        fecha =(EditText)findViewById(R.id.dtFechaNac);
+        telefono = (EditText) findViewById(R.id.txtTelefono);
+
+        if(nombre.getText().toString().length() > 0){
+            if(fecha.getText().toString().length() > 0){
+                String[] parts = fecha.getText().toString().split("/");
+                int año = Integer.parseInt(parts[2]);
+                if(año < LocalDate.now().getYear()){
+                    if(telefono.getText().toString().length() > 0){
+                        if(telefono.getText().toString().length() == 10){
+                            b= new Bundle();
+                            b.putString("NOMBRE", nombre.getText().toString());
+                            b.putString("SEXO", radGenero.getText().toString());
+                            b.putString("FECHA_NAC", fecha.getText().toString());
+                            b.putString("TELEFONO", telefono.getText().toString());
+
+                            intent.putExtras(b);
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(getBaseContext(), "El número de teléfono tiene que tener los diez dígitos", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    else{
+                        Toast.makeText(getBaseContext(), "Es obligatorio ingresar el número de teléfono", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    Toast.makeText(getBaseContext(), "El año de nacimiento tiene que ser menor al año actual", Toast.LENGTH_LONG).show();
+                }
+            }
+            else{
+                Toast.makeText(getBaseContext(), "Es obligatorio ingresar una fecha de nacimiento", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else{
+            Toast.makeText(getBaseContext(), "Es obligatorio ingresar su nombre", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
